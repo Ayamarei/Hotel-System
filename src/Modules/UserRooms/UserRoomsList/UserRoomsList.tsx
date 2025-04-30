30b205e9adc886107e63b922c10eb1be3c1d29a3
Samar Zeinah <samarzeinah@gmail.com>
2025-05-01 00:40:15 +0300

    updated explore

--- src/Modules/UserRooms/UserRoomsList/UserRoomsList.tsx
+++ src/Modules/UserRooms/UserRoomsList/UserRoomsList.tsx
@@ -10,7 +10,11 @@ import PaginationList from "../../Shared/PaginationList/PaginationList";
 import { FiltersContext } from "../../../context/FilterContext";
 import { THEMECOLOR } from "../../../Services/ThemeColors";
 import { ThemeContext } from "../../../context/ThemeContext";
-import PlaceholderImage from '../../../assets/images/explor.jpg'
+import PlaceholderImage from "../../../assets/images/explor.jpg";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { Link, useNavigate } from "react-router-dom";
 export interface Facility {
   _id: string;
   name: string;
@@ -49,24 +53,24 @@ export default function UserRoomsList() {
   const [roomData, setRoomData] = useState<RoomsAPIResponse | null>(null);
   const [page, setpage] = useState(1);
   const [totalCount, setTotalCount] = useState<number>(1);
-  console.log("Total Rooms Count:", totalCount);
-  // console.log("Calculated total pages:", Math.ceil(totalCount / 5));
+  const [likedRooms, setLikedRooms] = useState<string[]>([]);
+  console.log("likedRooms", likedRooms);
+  const navigate = useNavigate();
+
+  // filtersContext
   const filtersContext = useContext(FiltersContext);
   if (!filtersContext) {
     throw new Error("FiltersContext is not provided");
   }
   const { startDate, endDate, capacity } = filtersContext;
 
+  // ContextColor
   const ContextColor = useContext(ThemeContext);
   if (!ContextColor)
     throw new Error("AuthContext must be used within AuthProvider");
   const { theme } = ContextColor;
-  console.log("startDate1", startDate);
-  console.log("endDate1", startDate);
-  console.log("capacity1", startDate);
 
-  console.log("roomData", roomData);
-
+  //FetchRoomData
   const FetchRoomData = async (
     size?: number,
     page?: number,
@@ -105,11 +109,25 @@ export default function UserRoomsList() {
   };
 
   useEffect(() => {
-    FetchRoomData(6, page, startDate, endDate, capacity);
+    FetchRoomData(12, page, startDate, endDate, capacity);
   }, [page, startDate, endDate, capacity]);
+
+  //fun toggleLike
+  const toggleLike = (roomId: string) => {
+    let newLikedRooms = [...likedRooms];
+
+    if (newLikedRooms.includes(roomId)) {
+      newLikedRooms = newLikedRooms.filter((id) => id !== roomId);
+    } else {
+      newLikedRooms.push(roomId);
+    }
+
+    setLikedRooms(newLikedRooms);
+  };
 
   return (
     <>
+  
       <Typography
         variant="h4"
         sx={{
@@ -123,6 +141,25 @@ export default function UserRoomsList() {
       >
         Explore ALL Rooms
       </Typography>
+      
+
+      <Box sx={{ display: 'flex', alignItems: 'center' , justifyContent: {
+      xs: 'center', 
+      md: 'flex-start' 
+    },}}>
+      <Typography sx={{ fontSize: '16px', color: 'primary.main' }}>
+        <Link to="/" style={{ textDecoration: 'none', color: 'rgba(176, 176, 176, 1)' }}>
+          Home
+        </Link>
+      </Typography>
+      <Typography sx={{ fontSize: '16px', marginX: 1 ,color:"rgba(176, 176, 176, 1)"}}> / </Typography>
+      <Typography sx={{ fontSize: '16px', color: 'primary.main' }}>
+        <Link to="/user-room" style={{ textDecoration: 'none', color: theme === "dark" ? THEMECOLOR.mainBlue : THEMECOLOR.LabelColor, }}>
+          Explore
+        </Link>
+      </Typography>
+    </Box>
+
       <Box sx={{ flexGrow: 1, mt: "30px" }}>
         <Grid
           container
@@ -131,26 +168,71 @@ export default function UserRoomsList() {
           sx={{ justifyContent: "center" }}
         >
           {roomData?.data.rooms.map((room, index) => (
-            <Grid key={index} size={{ xs: 12, sm: 6, md: 6, lg: 4 }}>
+            <Grid key={index} size={{ xs: 12, sm: 4, md: 4, lg: 3 }}>
+
               <Card
                 sx={{
-                  maxWidth: 350,
+                  // maxWidth: 350,
                   margin: "auto",
                   borderRadius: "16px",
                   boxShadow: "0 4px 16px rgba(0,0,0,0.1)",
-                  transition: "transform 0.3s, box-shadow 0.3s",
+                  transition: "transform 0.5s, box-shadow 0.3s",
                   "&:hover": {
                     transform: "translateY(-5px)",
-                    boxShadow: theme === "dark"
-                      ? "0 6px 20px rgba(255, 255, 255, 0.2)"
-                      : "0 6px 20px rgba(0, 0, 0, 0.2)",
+                    boxShadow:
+                      theme === "dark"
+                        ? "0 6px 20px rgba(255, 255, 255, 0.2)"
+                        : "0 6px 20px rgba(0, 0, 0, 0.2)",
                     ".hover-overlay": {
                       opacity: 1,
+                      
                     },
+                    ".icon": {
+                        opacity: 1,    
+                      },
                   },
                 }}
               >
                 <Box sx={{ position: "relative" }}>
+                  {/* fav icon */}
+                  <Box
+                     className="icon"
+                    sx={{
+                      position: "absolute",
+                      top: "50%",
+                      right: "52%",
+                      zIndex: 10,
+                      cursor: "pointer",
+                      opacity: 0, 
+                      transition: "opacity 0.3s ease", 
+                    }}
+                    onClick={() => toggleLike(room._id)}
+                  >
+                    {likedRooms.includes(room._id) ? (
+                      <FavoriteIcon sx={{ color: "white", fontSize: 28 }} />
+                    ) : (
+                      <FavoriteBorderIcon
+                        sx={{ color: "white", fontSize: 28 }}
+                      />
+                    )}
+                  </Box>
+                  {/* view icon */}
+                  <Box
+                     className="icon"
+                    sx={{
+                      position: "absolute",
+                      top: "50%",
+                      left: "52%",
+                      zIndex: 10,
+                      cursor: "pointer",
+                      opacity: 0, // بشكل افتراضي الإيقونة مخفية
+                      transition: "opacity 0.3s ease", // تأثير الظهور
+                    }}
+                    // onClick={() => navigate(`/room-details/${room._id}`)}
+                  >
+                    <VisibilityIcon sx={{ color: "white", fontSize: 28 }} />
+                  </Box>
+
                   <Box
                     className="hover-overlay"
                     sx={{
@@ -159,7 +241,10 @@ export default function UserRoomsList() {
                       left: 0,
                       width: "100%",
                       height: "100%",
-                      bgcolor: theme === "dark" ? "rgba(255, 255, 255, 0.4)" : "rgba(0,0,0,0.5)",
+                      bgcolor:
+                        theme === "dark"
+                          ? "rgba(255, 255, 255, 0.2)"
+                          : "rgba(0,0,0,0.5)",
                       opacity: 0,
                       transition: "opacity 0.3s ease",
                       zIndex: 2,
@@ -175,12 +260,18 @@ export default function UserRoomsList() {
                         right: 0,
                         backgroundColor: "rgba(255, 73, 139, 1)",
                         color: "white",
-                        padding: "6px 12px",
+                        padding: {
+                          xs: "4px 10px",    
+                          sm: "6px 12px",   
+                          md: "6px 30px",   
+                        },
+                        textAlign: "center",
                         borderRadius: "0 10px 0 30px",
                         fontWeight: "500",
                         fontSize: "14px",
                         zIndex: 100,
                         width: "30%",
+                          whiteSpace: "nowrap"
                       }}
                     >
                       ${room.price} per night
@@ -196,32 +287,48 @@ export default function UserRoomsList() {
                     loop={true}
                   >
                     {room.images && room.images.length > 0 ? (
-                        room.images.map((img, imgIndex) => (
-                          <SwiperSlide key={imgIndex}>
-                            <CardMedia
-                              component="img"
-                              height="200"
-                              image={img}
-                              alt={`Room ${room.roomNumber} Image ${imgIndex + 1}`}
-                              sx={{ objectFit: "cover" }}
-                            />
-                          </SwiperSlide>
-                        ))
-                      ) : (
-                        <SwiperSlide>
+                      room.images.map((img, imgIndex) => (
+                        <SwiperSlide key={imgIndex}>
                           <CardMedia
                             component="img"
                             height="200"
-                            image={PlaceholderImage}
-                            alt="Placeholder Image"
+                            image={img}
+                            alt={`Room ${room.roomNumber} Image ${imgIndex + 1}`}
                             sx={{ objectFit: "cover" }}
                           />
                         </SwiperSlide>
-                      )}
+                      ))
+                    ) : (
+                      <SwiperSlide>
+                        <CardMedia
+                          component="img"
+                          height="200"
+                          image={PlaceholderImage}
+                          alt="Placeholder Image"
+                          sx={{ objectFit: "cover" }}
+                        />
+                      </SwiperSlide>
+                    )}
                   </Swiper>
+                  {/* Adding Room Number and Item Location */}
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        bottom: "10px",
+                        left: "10px",
+                        color: "#fff", 
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                        display: "flex",
+                        flexDirection: "column",
+                        zIndex: 20,
+                      }}
+                    >
+                      <Typography> {room.roomNumber}</Typography>
+                      <Typography>Item Location</Typography>
+                    </Box>
                 </Box>
               </Card>
-
             </Grid>
           ))}
         </Grid>
@@ -229,7 +336,7 @@ export default function UserRoomsList() {
         <PaginationList
           page={page}
           getAllList={(size, page) => FetchRoomData(size, page)}
-          totalCount={Math.ceil(totalCount / 6)}
+          totalCount={Math.ceil(totalCount / 12)}
           setpage={setpage}
           userExplore={true}
         />
